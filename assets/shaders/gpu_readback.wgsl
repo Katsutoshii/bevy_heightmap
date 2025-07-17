79ae92a229104f4dd44f3f1f82a29956b40d8042
Josiah Putman <joshikatsu@gmail.com>
2025-07-17 14:55:34 -0700

    Create compute shader readback example

--- assets/shaders/gpu_readback.wgsl
+++ assets/shaders/gpu_readback.wgsl
@@ -1,12 +1,45 @@
-// This shader is used for the gpu_readback example
-// The actual work it does is not important for the example
+#import "shaders/perlin_noise_2d.wgsl"::{perlin_noise_2d};
 
-// This is the data that lives in the gpu only buffer
-@group(0) @binding(0) var texture: texture_storage_2d<rgba32uint, write>;
+// Compute shader that populates a texture.
+@group(0) @binding(0) var texture: texture_storage_2d<rgba32float, write>;
 
+// Function to return a color for a given (x, y) point.
+fn f(xy: vec2<f32>) -> vec4<f32> {
+    let offset_xy = vec2<f32>(200.0, 100.0);
+
+    var output_color = vec4<f32>(1.0, 1.0, 1.0, 1.0);
+
+    let noise1_res = 128.0;
+    let noise1_weight = 3.0;
+    let noise1 = perlin_noise_2d(xy / noise1_res) * noise1_weight;
+
+    let noise2_res = 64.0;
+    let noise2_weight = 2.0;
+    let noise2 = perlin_noise_2d(xy / noise2_res) * noise2_weight;
+    
+    let noise3_res = 16.0;
+    let noise3_weight = 1.0;
+    let noise3 = perlin_noise_2d(xy / noise3_res) * noise3_weight;
+
+    let noise4_res = 8.0;
+    let noise4_weight = 1.0;
+    let noise4 = perlin_noise_2d(xy / noise4_weight) * noise4_weight;
+
+    let noise = (
+      (noise1 + noise2 + noise3 + noise4) /
+      (noise1_weight + noise2_weight + noise3_weight + noise4_weight)
+    );
+    let noise_amount = 2.0;
+
+    output_color.r *= noise * noise_amount;
+    return output_color;
+}
+
+// Writes the function value to each pixel of the texture.
 @compute @workgroup_size(1)
 fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    let texture_xy = vec2<i32>(i32(global_id.x), i32(global_id.y));
-    let color = vec4<u32>(global_id.x, global_id.y, global_id.z, 0);
-    textureStore(texture, texture_xy, color);
+    let texture_xy = vec2<i32>(global_id.xy);
+    let xy = vec2<f32>(global_id.xy) * 1.0;
+    let offset_xy = vec2<f32>(100.0, 100.0);
+    textureStore(texture, texture_xy, f(xy + offset_xy));
 }
--- examples/value_function_readback.rs
+++ examples/value_function_readback.rs
@@ -1,87 +1,182 @@
+use std::f32::consts::PI;
+
+use bevy_compute_readback::{ComputeShader, ComputeShaderPlugin, ReadbackLimit};
 use bevy_ecs::world::DeferredWorld;
-// Uses GPU readback to get the results from a compute shader.
-use bevy_heightmap::compute_shader::{
-    ComputeShader, ComputeShaderPlugin, ComputeShaderReadback, ReadbackLimit,
-};
 
 use bevy::{
     asset::RenderAssetUsages,
+    color::palettes::css::GRAY,
     prelude::*,
     render::{
         extract_resource::ExtractResource,
-        gpu_readback::Readback,
+        gpu_readback::{Readback, ReadbackComplete},
         render_resource::{
             AsBindGroup, Extent3d, ShaderRef, TextureDimension, TextureFormat, TextureUsages,
         },
     },
 };
-use bevy_render::gpu_readback::ReadbackComplete;
+use bevy_heightmap::{HeightMap, HeightMapPlugin};
+use bevy_image::TextureFormatPixelInfo;
+use image::Rgba32FImage;
+
+pub const SCALE: f32 = 1024.;
+pub const HEIGHT: f32 = 32.;
+pub const THETA: f32 = PI / 8.;
+pub const FOV: f32 = PI / 4.;
+pub fn y_offset(z: f32) -> f32 {
+    THETA.tan() * z
+}
 
 fn main() {
     let mut app = App::new();
     app.add_plugins((
         DefaultPlugins,
-        ComputeShaderPlugin::<CustomComputeShader>::default(),
+        HeightMapPlugin,
+        ComputeShaderPlugin::<NoiseComputeShader> {
+            limit: ReadbackLimit::Finite(1),
+            remove_on_complete: false,
+            ..default()
+        },
     ))
     .insert_resource(ClearColor(Color::BLACK))
     .add_systems(Startup, setup)
     .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(ComputeShaderReadback::<CustomComputeShader> {
-        limit: ReadbackLimit::Finite(1),
-        ..default()
-    });
+fn setup(
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut commands: Commands,
+    noise_shader: ResMut<NoiseComputeShader>,
+) {
+    let texture: Handle<Image> = asset_server.load("textures/uv.png");
+    commands.spawn((
+        Name::new("Terrain"),
+        Mesh3d(noise_shader.generated_mesh.clone()),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            base_color_texture: Some(texture),
+            ..default()
+        })),
+        Transform {
+            scale: Vec2::splat(SCALE).extend(HEIGHT),
+            ..default()
+        },
+    ));
+    commands.spawn((
+        Name::new("Origin"),
+        Mesh3d(meshes.add(Cuboid {
+            half_size: Vec3::ONE * 0.5,
+        })),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            ..default()
+        })),
+        Transform {
+            scale: 10. * Vec3::ONE,
+            ..default()
+        },
+    ));
+    commands.spawn((
+        Name::new("One"),
+        Mesh3d(meshes.add(Cuboid {
+            half_size: Vec3::ONE * 0.5,
+        })),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: GRAY.into(),
+            ..default()
+        })),
+        Transform {
+            scale: 10. * Vec3::ONE,
+            translation: (Vec2::ONE * SCALE / 2.).extend(0.),
+            ..default()
+        },
+    ));
+    let default_height = 1500.;
+    commands.spawn((
+        Camera3d::default(),
+        Projection::Perspective(PerspectiveProjection {
+            fov: FOV,
+            near: 0.1,
+            far: 2000.,
+            ..default()
+        }),
+        Transform::from_xyz(0.0, -y_offset(default_height), default_height)
+            .with_rotation(Quat::from_axis_angle(Vec3::X, THETA)),
+    ));
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 4500.,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, default_height)
+            .with_rotation(Quat::from_axis_angle(Vec3::ONE, -PI / 6.)),
+    ));
 }
 
 // This is the struct that will be passed to your shader
 #[derive(AsBindGroup, Resource, Clone, Debug, ExtractResource)]
-pub struct CustomComputeShader {
-    #[storage_texture(0, image_format=Rgba32Uint, access=WriteOnly)]
+pub struct NoiseComputeShader {
+    #[storage_texture(0, image_format=Rgba32Float, access=WriteOnly)]
     texture: Handle<Image>,
+
+    // Mesh computed from the readback texture.
+    generated_mesh: Handle<Mesh>,
 }
-impl ComputeShader for CustomComputeShader {
+impl ComputeShader for NoiseComputeShader {
     fn compute_shader() -> ShaderRef {
         "shaders/gpu_readback.wgsl".into()
     }
     fn workgroup_size() -> UVec3 {
-        UVec3::new(16, 16, 1)
+        UVec3::new(1024, 1024, 1)
     }
-    fn readbacks(&self) -> impl Bundle {
-        Readback::texture(self.texture.clone())
+    fn readback(&self) -> Option<Readback> {
+        Some(Readback::texture(self.texture.clone()))
     }
     fn on_readback(trigger: Trigger<ReadbackComplete>, mut world: DeferredWorld) {
-        let data: Vec<u8> = trigger.event().0.clone();
-        info!("Data len: {}", data.len());
-        let image_handle = world.resource::<Self>().texture.clone();
-        let mut images = world.resource_mut::<Assets<Image>>();
-        if let Some(image) = images.get_mut(&image_handle) {
-            image.data = Some(data);
-        } else {
-            warn!("Handle not ready: {:?}", image_handle);
-        }
+        let size = Self::workgroup_size().xy();
+        let bounds = size - UVec2::ONE;
+        let pixel_scale = bounds.as_vec2();
+        let rgba = Rgba32FImage::from_raw(
+            size.x,
+            size.y,
+            bytemuck::cast_slice(&trigger.event().0).to_owned(),
+        )
+        .unwrap();
+        let h = |p: Vec2| -> f32 {
+            let xy = (pixel_scale * (p + Vec2::ONE / 2.)).as_uvec2();
+            rgba.get_pixel(xy.x, bounds.y - xy.y)[0]
+        };
+
+        let mesh_handle = world.resource::<Self>().generated_mesh.clone();
+        let mut meshes = world.resource_mut::<Assets<Mesh>>();
+        let Some(mesh) = meshes.get_mut(&mesh_handle) else {
+            warn!("Handle not ready: {:?}", mesh_handle);
+            return;
+        };
+        *mesh = HeightMap { size, h }.into();
     }
 }
-impl FromWorld for CustomComputeShader {
+impl FromWorld for NoiseComputeShader {
     fn from_world(world: &mut World) -> Self {
-        let workgroup_size = Self::workgroup_size();
-        let size = Extent3d {
-            width: workgroup_size.x,
-            height: workgroup_size.y,
-            depth_or_array_layers: workgroup_size.z,
-        };
-        let empty_pixel: Vec<u8> = vec![0; 8 * 4];
-        let mut image = Image::new_fill(
-            size,
+        let size = Self::workgroup_size();
+        let mut image = Image::new(
+            Extent3d {
+                width: size.x,
+                height: size.y,
+                depth_or_array_layers: size.z,
+            },
             TextureDimension::D2,
-            &empty_pixel,
-            TextureFormat::Rgba32Uint,
+            vec![0; TextureFormat::Rgba32Float.pixel_size() * size.x as usize * size.y as usize],
+            TextureFormat::Rgba32Float,
             RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
         );
         image.texture_descriptor.usage |= TextureUsages::COPY_SRC | TextureUsages::STORAGE_BINDING;
         Self {
             texture: world.add_asset(image),
+            generated_mesh: world.add_asset(Cuboid::default()),
         }
     }
 }
